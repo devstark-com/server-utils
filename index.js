87ea1ce22e798a60142b006121e0cdfc3f4aa698
SinaniG <SinaniG1996@gmail.com>
2019-09-12 18:50:25 +0300

    Add get token from header util

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const errors = require('./src/errors')
 const cleanStackTrace = require('./src/clean-stack-trace')
+const getTokenFromHeader = require('./src/get-token-from-header')
 const { auth, errorHandler } = require('./src/koa')
 
 module.exports = {
   errors,
   cleanStackTrace,
+  getTokenFromHeader,
   koa: { auth, errorHandler }
 }
